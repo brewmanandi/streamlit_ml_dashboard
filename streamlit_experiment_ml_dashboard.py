79280009e5d78e431f15ba58e73772b6aa940c72
Andreas Braumann <brewman@gmx.at>
2024-07-03 17:21:08 +0200

    - update ui - add download model functionality

--- streamlit_experiment_ml_dashboard.py
+++ streamlit_experiment_ml_dashboard.py
@@ -1,3 +1,6 @@
+from io import BytesIO
+
+import joblib
 import matplotlib.pyplot as plt
 import scikitplot as skplt
 import streamlit as st
@@ -18,6 +21,7 @@ def wide_space_default():
 wide_space_default()
 
 categories = ['alt.atheism', 'comp.graphics', 'rec.autos', 'sci.electronics', 'talk.politics.guns',]
+models = []
 
 @st.cache_data
 def load_and_vectorize_dataset():
@@ -34,16 +38,15 @@ def load_and_vectorize_dataset():
 
 X_train_vec, X_test_vec, Y_train, Y_test = load_and_vectorize_dataset()
 
-def train_models(n_estimators, max_depth, max_features, bootstrap):
-    models = []
-    models.append(RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, max_features=max_features, bootstrap=bootstrap))
-    models.append(DecisionTreeClassifier(max_depth=max_depth, max_features=max_features))
-    models.append(GradientBoostingClassifier(n_estimators=n_estimators, max_depth=max_depth, max_features=max_features))
+def train_models(n_estimators, max_depth, max_features, min_samples_split, min_samples_leaf, max_leaf_nodes, min_impurity_decrease):
+    # Adjust the parameters for each classifier according to their support
+    models.append(RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, max_features=max_features, min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf, max_leaf_nodes=max_leaf_nodes, min_impurity_decrease=min_impurity_decrease))
+    models.append(DecisionTreeClassifier(max_depth=max_depth, max_features=max_features, min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf, max_leaf_nodes=max_leaf_nodes, min_impurity_decrease=min_impurity_decrease))
+    models.append(GradientBoostingClassifier(n_estimators=n_estimators, max_depth=max_depth, max_features=max_features, min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf, max_leaf_nodes=max_leaf_nodes, min_impurity_decrease=min_impurity_decrease))
 
     # Fit each model
     for model in models:
         model.fit(X_train_vec, Y_train)
-    return models
 
 ## Dashboard
 st.title("Classifier :green[Experiments] :tea: :coffee:")
@@ -60,64 +63,76 @@ def set_submitted():
 
 col1, col2 = st.columns(2, gap="medium")
 
+
 with col1:
     n_estimators = st.slider("No of Estimators", min_value=100, max_value=1000, on_change=set_submitted)
     max_depth = st.slider("Max Depth", min_value=2, max_value=20, on_change=set_submitted)
-    max_features = st.selectbox("Max Features", options=["sqrt", "log2", None], on_change=set_submitted)
-
+    min_samples_split = st.slider("Min Samples Split", min_value=2, max_value=20, on_change=set_submitted)
+    min_samples_leaf = st.slider("Min Samples Leaf", min_value=1, max_value=20, on_change=set_submitted)
 with col2:
-    bootstrap = st.checkbox("Bootstrap")
-    save_model = st.checkbox("Save Model")
-
+    max_leaf_nodes = st.slider("Max Leaf Nodes", min_value=10, max_value=1000, value=None, on_change=set_submitted)
+    min_impurity_decrease = st.slider("Min Impurity Decrease", min_value=0.0, max_value=0.5, step=0.01, on_change=set_submitted)
+    max_features = st.selectbox("Max Features", options=["sqrt", "log2", None], on_change=set_submitted)    
 
 
 if st.session_state.submitted:
     with col2:
         with st.spinner('Training models, please wait...'):
-            models = train_models(n_estimators, max_depth, max_features, bootstrap)
+            models = []
+            train_models(n_estimators, max_depth, max_features, min_samples_split, min_samples_leaf, max_leaf_nodes, min_impurity_decrease)
             st.success('Model training completed!')
 
-    if save_model:
-        file_name = "models.dat"
-        dump(models, file_name)
-        st.toast(f"Models Saved Successfully to {file_name}!", icon="💾")
+            file_name = "models.dat"
+            buffer = BytesIO()
+            joblib.dump(models, buffer)
+            buffer.seek(0)
 
-    columns = st.columns(len(models), gap="medium")
-    for i, model in enumerate(models):
-        with columns[i]:
-            st.markdown(f"# {model.__class__.__name__}")
-            Y_test_preds = model.predict(X_test_vec)
-            Y_train_preds = model.predict(X_train_vec)
-            Y_test_probs = model.predict_proba(X_test_vec)
+            st.download_button(
+                label="Download Models",
+                data=buffer,
+                file_name="models.pkl",
+                mime="application/octet-stream"
+            )
+            st.toast(f"Models Successfully Downloaded!", icon="💾")            
 
-            col21, col22 = st.columns(2, gap="medium")
-            with col21:
-                st.metric("Test Accuracy", value="{:.2f} %".format(100*accuracy_score(Y_test, Y_test_preds)))
-            with col22:
-                st.metric("Train Accuracy", value="{:.2f} %".format(100*accuracy_score(Y_train, Y_train_preds)))
+    
+    if len(models) > 0:
+        columns = st.columns(len(models), gap="medium")
+        for i, model in enumerate(models):
+            with columns[i]:
+                st.markdown(f"# {model.__class__.__name__}")
+                Y_test_preds = model.predict(X_test_vec)
+                Y_train_preds = model.predict(X_train_vec)
+                Y_test_probs = model.predict_proba(X_test_vec)
 
-            st.markdown("### Confusion Matrix")
-            conf_mat_fig = plt.figure(figsize=(6,6))
-            ax1 = conf_mat_fig.add_subplot(111)
-            skplt.metrics.plot_confusion_matrix(Y_test, Y_test_preds, ax=ax1)
-            st.pyplot(conf_mat_fig, use_container_width=True)
+                col21, col22 = st.columns(2, gap="medium")
+                with col21:
+                    st.metric("Test Accuracy", value="{:.2f} %".format(100*accuracy_score(Y_test, Y_test_preds)))
+                with col22:
+                    st.metric("Train Accuracy", value="{:.2f} %".format(100*accuracy_score(Y_train, Y_train_preds)))
 
-            st.markdown("### Classification Report:")
-            st.code("=="+classification_report(Y_test, Y_test_preds, target_names=categories))
+                st.markdown("### Confusion Matrix")
+                conf_mat_fig = plt.figure(figsize=(6,6))
+                ax1 = conf_mat_fig.add_subplot(111)
+                skplt.metrics.plot_confusion_matrix(Y_test, Y_test_preds, ax=ax1)
+                st.pyplot(conf_mat_fig, use_container_width=True)
 
-            st.markdown("### ROC & Precision-Recall Curves")
-            col1, col2 = st.columns(2, gap="medium")
-            with col1:
-                roc_fig = plt.figure(figsize=(6,6))
-                ax1 = roc_fig.add_subplot(111)
-                skplt.metrics.plot_roc(Y_test, Y_test_probs, ax=ax1)
-                st.pyplot(roc_fig, use_container_width=True)
+                st.markdown("### Classification Report:")
+                st.code("=="+classification_report(Y_test, Y_test_preds, target_names=categories))
 
-            with col2:
-                pr_fig = plt.figure(figsize=(6,6))
-                ax1 = pr_fig.add_subplot(111)
-                skplt.metrics.plot_precision_recall(Y_test, Y_test_probs, ax=ax1)
-                st.pyplot(pr_fig, use_container_width=True)
+                st.markdown("### ROC & Precision-Recall Curves")
+                col1, col2 = st.columns(2, gap="medium")
+                with col1:
+                    roc_fig = plt.figure(figsize=(6,6))
+                    ax1 = roc_fig.add_subplot(111)
+                    skplt.metrics.plot_roc(Y_test, Y_test_probs, ax=ax1)
+                    st.pyplot(roc_fig, use_container_width=True)
+
+                with col2:
+                    pr_fig = plt.figure(figsize=(6,6))
+                    ax1 = pr_fig.add_subplot(111)
+                    skplt.metrics.plot_precision_recall(Y_test, Y_test_probs, ax=ax1)
+                    st.pyplot(pr_fig, use_container_width=True)
 
     st.session_state.submitted = False
 
